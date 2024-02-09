0847c82c249270fda619a79f399436215fdb475b
fm16191 <56755856+fm16191@users.noreply.github.com>
2024-02-09 12:01:42 +0100

    Add scripts to test all oneapi available versions

---
+++ Makefile/scripts/gen_tests_arria_hw.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# 2022.3.1 NOPE
+# 2024.0.2.1 NOPE
+# 2024.0.2 NOPE
+# 2024.0 NOPE
+for version in $(echo "2023.2.0.1 2023.0.1 2023.2.0.2_s 2023.0 2023.2.0 2023.1.0.4 2023.1.1_cmake 2023.1.1 2023.1.2 test" | xargs); do
+
+fn="test_run_arria_hw_${version}.sh"
+rm -f $fn
+touch $fn && chmod +x $fn
+
+cat <<EOT | tee -a $fn > /dev/null
+#!/bin/bash
+
+#PBS -l nodes=1:fpga:fpga_runtime:arria10:ppn=2
+#PBS -d .
+
+oneapi_version=$version
+
+source /glob/development-tools/versions/oneapi/\$oneapi_version/oneapi/setvars.sh --force
+
+TARGET=build-arria-\$oneapi_version
+# BOARD_NAME=intel_a10gx_pac:pac_a10
+
+# prepare env
+mkdir -p \$TARGET
+cp -r Makefile.fpga *.s src \$TARGET
+cd \$TARGET
+
+# sed -i "s/\(\-Xsboard\).*$/\1=\${BOARD_NAME}/g" Makefile.fpga
+
+echo
+echo start: \$(date "+%y/%m/%d %H:%M:%S.%3N")
+echo
+
+make -f Makefile.fpga clean
+make -f Makefile.fpga fpga_emu && echo "EMU OK" || echo "EMU NOPE"
+make -f Makefile.fpga report && echo "REPORT OK" || echo "REPORT NOPE"
+(make -f Makefile.fpga hw && echo "HW OK" || echo "HW NOPE") && (make -f Makefile.fpga run_hw && echo "RUN_HW OK" || echo "RUN_HW NOPE")
+
+echo
+echo stop: \$(date "+%y/%m/%d %H:%M:%S.%3N")
+echo
+EOT
+
+done
